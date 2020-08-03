e95c8c7e28cec251ed061d0bb687da00d5076554
raisuahmed <raisuahmed83@gmail.com>
2020-08-03 12:21:01 -0700

    check and working

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ function deleteButton() {
 function clearEverything() {
     document.getElementById('input-generatedPin').value = "";
 }
+
+// Active the submit Button
+
+function submitButton() {
+    let inputNumber = document.getElementById('input-generatedPin').value;
+    let generatedNumber = document.getElementById('generated-pin').value;
+    if (inputNumber == "" && generatedNumber == "") {
+        alert('Please Generate Pin First');
+    } else if (inputNumber == generatedNumber) {
+        document.getElementById("matched-notify").style.display = "block"
+        document.getElementById("misMatch-notify").style.display = "none"
+    } else {
+        document.getElementById("matched-Notify").style.display = "none"
+        document.getElementById("misMatch-notify").style.display = "block"
+        tryLeft();
+    }
+}
