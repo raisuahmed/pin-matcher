09db74fb16b87f7fe9298e68b47ca4d8b83586e6
raisuahmed <raisuahmed83@gmail.com>
2020-08-03 11:58:20 -0700

    doing work on function

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 document.getElementById("matched-notify").style.display = "none";
 document.getElementById("misMatch-notify").style.display = "none";
 document.getElementById("action-left").style.display = "none";
-// Generating random Number
+
+// Generate the random Number
 function generateRandom() {
     document.getElementById('generated-pin').value = Math.floor(Math.random() * 8999 + 1000);
 }
-// ACTIVE THE NUMBER BUTTONS
+// active the number buttons
 function numberButton(num) {
     document.getElementById('input-generatedPin').value = document.getElementById('input-generatedPin').value + num;
 }
 // active the delete button
-function deleteBtn() {
+function deleteButton() {
     let inputValue = document.getElementById('input-generatedPin').value;
     document.getElementById('input-generatedPin').value = inputValue.substring(0, inputValue.length - 1);
 }
