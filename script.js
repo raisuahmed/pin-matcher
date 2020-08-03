adf189934ba34bc4b5e74425ae09dc2cdbb5c01e
raisuahmed <raisuahmed83@gmail.com>
2020-08-03 14:12:09 -0700

    doing work without beep

--- script.js
+++ script.js
@@ -18,24 +18,50 @@ function deleteButton() {
 function clearEverything() {
     document.getElementById('input-generatedPin').value = "";
 }
-// the notification invisible
+//the notification invisible
 document.getElementById("matched-notify").style.display = "none";
 document.getElementById("misMatch-notify").style.display = "none";
-document.getElementById("action-left").style.display = "none";
+document.getElementById("action-left").style.display = "block";
 
 // active the submit Button
 
-function submitButton() {
-    let inputNumber = document.getElementById('input-generatedPin').value;
-    let generatedNumber = document.getElementById('generated-pin').value;
-    if (inputNumber == "" && generatedNumber == "") {
-        alert('Please Generate Pin First');
-    } else if (inputNumber == generatedNumber) {
+// function submitButton() {
+//     let inputNumber = document.getElementById('input-generatedPin').value;
+//     let generatedNumber = document.getElementById('generated-pin').value;
+//     if (inputNumber == "" && generatedNumber == "") {
+//         alert('Please Generate Pin First');
+//     } else if (inputNumber == generatedNumber) {
+//         document.getElementById("matched-notify").style.display = "block"
+//         document.getElementById("misMatch-notify").style.display = "none"
+//     } else {
+//         document.getElementById("matched-notify").style.display = "none"
+//         document.getElementById("misMatch-notify").style.display = "block"
+//         tryLeft();
+//     }
+// }
+
+
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+
+    let generatePin = document.getElementById('generated-pin').value;
+    let inputSubmitPin = document.getElementById('input-generatedPin').value
+    if (generatePin == inputSubmitPin) {
+        
         document.getElementById("matched-notify").style.display = "block"
         document.getElementById("misMatch-notify").style.display = "none"
-    } else {
-        document.getElementById("matched-notify").style.display = "none"
-        document.getElementById("misMatch-notify").style.display = "block"
-        tryLeft();
     }
-}
+    else {
+        alert('Please Input Current Pin');
+        document.getElementById("misMatch-notify").style.display = "block"
+        document.getElementById("matched-notify").style.display = "none"
+        
+        let totalActionLeft = parseInt(document.getElementById("action-left").innerText);
+        totalActionLeft -= 1;
+        document.getElementById("action-left").innerText = totalActionLeft;
+        if (totalActionLeft == 0) {
+            document.getElementById("submitBtn").disabled = true;
+            document.getElementById("submitBtn").style.backgroundColor = "#f01121";
+        }
+    }
+})
